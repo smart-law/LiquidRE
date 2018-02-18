73a86cea7ff599041ad70da5ada12dc6d1264e8e
Zach Doty <zach.doty@gmail.com>
2018-02-17 16:50:22 -0800

    buffer

---
+++ contracts/Buffer.sol
@@ -0,0 +1,35 @@
+pragma solidity ^0.4.16;
+
+contract Buffer {
+
+  uint bufferBalance;
+  uint blocksInPeriod = 170000;
+  uint startingBlock;
+
+  function Buffer(){
+
+  }
+
+  function Deposit(uint amount, address lret) payable{
+    transferFrom(address msg.sender, address lret, uint amount);
+  }
+
+
+  function getAvailableAmount() public returns(uint){
+    //return amount of buffer available to CRR
+    blocksSinceDeposit = block.number - startingBlock;
+
+    uint availableAmount = (blocksSinceDeposit / blocksInPeriod) * bufferBalance;
+
+    return availableAmount;
+
+  }
+  
+
+  function getUnavailableAmount(){
+    return bufferBalance - getAvailableAmount();
+  }
+
+
+
+}
